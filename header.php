6cf5c654b391092e9b90b73378c9d4a2dd949ada
koyasato <koya.sato.017@gmail.com>
2021-03-13 09:40:07 +0900

    fix: 18行目 get_main_title(); に修正

--- header.php
+++ header.php
@@ -15,12 +15,12 @@
         <a href="index.php">
           <img src="<?php echo get_template_directory_uri(); ?>/images/hikouki_gumo.png" alt="飛行機雲の画像" />
           <p>おひるねアート</p>
-          <h1 class="title"><?php echo get_the_title(); ?></h1>
+          <h1 class="title"><?php echo get_main_title(); ?></h1>
         </a>
 <?php if(is_front_page()): ?>
         <p class="site-introduction_description">愛知県安城市のおひるねアート&reg;認定講師　佐藤智加のブログです</p>
 <?php else: ?>
-  <p class="site-introduction_description">愛知県安城市のおひるねアート&reg;</p>
+        <p class="site-introduction_description"></p>
 <?php endif; ?>
       </div>
       <input id="my-parts-checkbox" class="my-parts-hidden" type="checkbox">
@@ -34,17 +34,5 @@ wp_nav_menu(
   )
 );
 ?>
-        <!-- <div class="gnavi__inner">
-          <ul class="nav">
-            <li><a href="index.php"><span>ホーム</span></a></li>
-            <li><a href="profile.php"><span>自己紹介</span></a></li>
-            <li><a href="photo_gallery.php"><span>フォトギャラリー</span></a></li>
-            <li><a href="line.php" target="_blank" rel="noopener noreferrer"><span>LINE</span></a></li>
-            <li><a href="https://www.instagram.com/ko_yay_ok/?hl=ja" target="_blank" rel="noopener noreferrer" ><span>Instagram</span></a></li>
-            <li><a href="https://www.facebook.com/koya.sato.017/" target="_blank" rel="noopener noreferrer"><span>Facebook</span></a></li>
-            <li><a href="https://twitter.com/home?lang=ja" target="_blank" rel="noopener noreferrer"><span>Twitter</span></a></li>
-            <li><a href="contact.php"><span>お問い合わせ</span></a></li>
-          </ul>
-        </div> -->
       </nav>
     </header>
