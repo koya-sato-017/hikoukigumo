b4cec3b0fa03b7f15565cd0bda8dfd8d8398f962
koyasato <koya.sato.017@gmail.com>
2021-03-18 06:16:32 +0900

    fix: 6,9行目修正

--- content-common.php
+++ content-common.php
@@ -1,11 +1,12 @@
 <li class="common-item">
     <a class="common-link" href="<?php the_permalink(); ?>">
-        <div class="common-image"><?php the_post_thumbnail(); ?></div>
+        <div class="common-image"><?php the_post_thumbnail('common'); ?></div>
         <div class="common-body">
             <p class="name"><?php the_title(); ?></p>
-            <p class="caption"><?php get_the_excerpt(); ?></p>
+            <time class="release"><?php the_time('Y.m.d'); ?></time>
+            <!-- <p class="caption"><?php get_the_excerpt(); ?></p> -->
             <div class="buttonBox">
-                <button type="button" class="seeDetail">MORE</button>
+                <button type="button" class="seeDetail">続きを読む</button>
             </div>
         </div>
     </a>
