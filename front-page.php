a01772396819bd0d62f1023aad00d4714bd6ec02
koyasato <koya.sato.017@gmail.com>
2021-03-22 06:09:20 +0900

    add: 40行目 第二引数を追加

--- front-page.php
+++ front-page.php
@@ -23,41 +23,42 @@
     </ul>
   </article>
 
-  <section class="section-contents" id="profile">
+  <section class="section-contents" id="about">
     <div class="wrapper">
 <?php
 $about_obj = get_page_by_path('about');
 $post = $about_obj;
 setup_postdata($post);
-$shop_title = get_the_title();
+$about_title = get_the_title();
 ?>
 
-<h2 class="section-title"><?php the_title(); ?></h2>
+    <h2 class="section-title"><?php the_title(); ?></h2>
+    <p class="section-lead"><?php echo get_the_excerpt(); ?></p>
 <?php wp_reset_postdata(); ?>
-  <ul class="about-info">
+    <ul class="about-info">
 <?php
-$about_pages = get_child_pages();
+$about_pages = get_child_pages(-1, $about_obj->ID);
 if($about_pages->have_posts()):
   while($about_pages->have_posts()): $about_pages->the_post();
 ?>
-    <li class="about-item">
-      <a class="about-link" href="<?php the_permalink(); ?>">
-        <div class="about-image"><?php the_about_thumbnail('common'); ?></div>
-        <div class="about-body">
-          <p class="name"><?php the_title(); ?></p>
-          <p class="location"></p>
-          <div class="buttonBox">
-            <button type="button" class="seeDerail">MORE</button>
+      <li class="about-item">
+        <a class="about-link" href="<?php the_permalink(); ?>">
+          <div class="about-image"><?php the_post_thumbnail('common'); ?></div>
+          <div class="about-body">
+            <p class="name"><?php the_title(); ?></p>
+            <p class="location"></p>
+            <div class="buttonBox">
+              <button type="button" class="seeDerail">MORE</button>
+            </div>
           </div>
-        </div>
-      </a>
-    </li>
+        </a>
+      </li>
 <?php
   endwhile;
   wp_reset_postdata();
 endif;
 ?>
-  </ul>
+    </ul>
 
 
     </div>
